275aab30c8b02cd59def379c23d1cb41e0e1b871
Leeo <mizzking75@gmail.com>
2026-01-23 17:55:31 +0900

    1.0.0 배포

---
+++ TodoAlarm/TodoAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAlarmApp.swift
+//  TodoAlarm
+//
+//  Created by Leeo on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
